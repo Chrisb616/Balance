efc12cba6998522b0148a0a93c80731d275be142
Chrisb616 <cboynton16@gmail.com>
2017-01-14 10:07:53 -0500

    Set up basic core data, set up programattic launch

---
+++ Balance/MenuViewController.swift
@@ -0,0 +1,32 @@
+//
+//  MenuViewController
+//  Balance
+//
+//  Created by Christopher Boynton on 1/12/17.
+//  Copyright © 2017 Christopher. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let user = User.shared
+        
+        user.loadFromCoreData()
+        
+        print(user.points)
+        
+        user.points += 1
+        
+        print(user.points)
+        
+        user.save()
+        
+    }
+
+
+}
+
---
+++ Balance/User.swift
@@ -0,0 +1,42 @@
+//
+//  User.swift
+//  Balance
+//
+//  Created by Christopher Boynton on 1/14/17.
+//  Copyright © 2017 Christopher. All rights reserved.
+//
+
+import Foundation
+
+class User {
+    
+    private init() {}
+    static let shared = User()
+    
+    var name = ""
+    var points = 0
+}
+
+extension User {
+    
+    func loadFromCoreData() {
+        
+        if let storedUser = CoreDataManager.storedUser {
+            
+            guard let name = storedUser.name else { return }
+            guard let pointsString = storedUser.points else { return }
+            guard let points = Int(pointsString) else { return }
+            
+            
+            self.name = name
+            self.points = points
+            
+        }
+        
+    }
+    
+    func save() {
+        CoreDataManager.saveUser(self)
+    }
+    
+}
